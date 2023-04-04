60cbace0aa75ad83b8a6afafc6b9ec699ce210a6
Mimi <mireiag77@gmail>
2023-04-04 14:36:10 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio_Secuencial_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce el primer factor");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduce el segundo factor");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("El resultado es: {0}", num1 * num2);
+        }
+    }
+}
